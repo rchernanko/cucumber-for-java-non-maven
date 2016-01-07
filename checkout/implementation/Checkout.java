d890dfbb41bc2ac80bbbf590285cf4149459466f
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-01-07 21:05:15 +0000

    initial commit

---
+++ checkout/implementation/Checkout.java
@@ -0,0 +1,15 @@
+package implementation;
+
+public class Checkout {
+
+    private int runningTotal = 0;
+
+    public void add(int count, int price) {
+        runningTotal += (count * price);
+    }
+
+    public int total() {
+        return runningTotal;
+    }
+
+}
